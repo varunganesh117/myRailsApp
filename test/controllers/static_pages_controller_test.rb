8378a2438c3b68f577972bf97828828a1621bc6d
varunganesh91 <operationgenesis@gmail.com>
2016-04-21 08:04:54 +0000

    Add contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@base_title}"
   end
   
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "h1", "Contact"
+  end
+  
 end
