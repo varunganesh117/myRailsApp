6d6d4614ba8e184d338f2389116aec9dcb16e6d5
varunganesh91 <operationgenesis@gmail.com>
2016-04-22 03:30:54 +0000

    Add helper method for title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(title="")
+    default = "Ruby on Rails Tutorial Sample App"
+    if title.empty?
+      return default
+    else
+      return title + " | " + default
+    end
+  end
 end
