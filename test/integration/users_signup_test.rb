9cceb4b9cebec45ee603ff545d2e41a0b36fa75a
varunganesh91 <operationgenesis@gmail.com>
2016-07-09 15:44:33 +0000

    Tests for account activation and mailer

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+  
   test "invalid signup information" do
     get signup_path
     assert_template 'users/new'
@@ -20,14 +25,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_template 'users/new'
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name: "ABC User",
+      post users_path, user: { name: "ABC User",
                                email: "user@example.com",
                                password: "foobar",
                                password_confirmation: "foobar" }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: "dummy")
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
     assert_not_nil flash[:success]
     assert_select 'div.alert-success'
-    assert is_logged_in?
   end
 end
