e7099d89f09f4c2c83dff92f842a85ed8e924cfb
varunganesh91 <operationgenesis@gmail.com>
2016-07-09 15:44:59 +0000

    Implementation for account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, 
@@ -9,16 +10,38 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
-  #Return hash digest of plain text password
+  #Return hash digest of plain text
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
   
+  # Converts email to all lower-case.
+  def downcase_email
+    self.email.downcase!
+  end
+  
+  #Activate account
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+  
+  #Sends email with activation link
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  
   #Returns a random token
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+  
+  #Creates and assigns the activation token and digest
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
   
   #Remembers a user in the database for persistent sessions
@@ -28,9 +51,10 @@ class User < ActiveRecord::Base
   end
   
   #Returns true if given token matches the stored digest
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
   
   #Forgets a user
