a7562d71369364a6681968e6b9492f8f2e03aa4a
varunganesh91 <operationgenesis@gmail.com>
2016-07-10 06:24:26 +0000

    Restrict show/index action to activated users

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,7 @@ class User < ActiveRecord::Base
   
   #Activate account
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
   
   #Sends email with activation link
